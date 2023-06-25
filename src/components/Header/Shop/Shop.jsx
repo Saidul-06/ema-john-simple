b8349df465f11f347fd7e1a342390f94201d666e
Saidul-06 <saidul15-2703@diu.edu.bd>
2023-06-25 14:20:09 +0600

    correction of component

--- src/components/Header/Shop/Shop.jsx
+++ src/components/Header/Shop/Shop.jsx
@@ -5,11 +5,19 @@ import Product from '../../Product/Product';
 
 const Shop = () => {
     const [products,setProducts] = useState ([]);
+    const [cart,setCart]= useState([]);
     useEffect(() =>{
         fetch ('products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
     },[])
+
+    const handleAddToCart = (product) =>
+    {      
+        const newCart = [...cart,product]
+        setCart(newCart);  
+    } 
+
     return (
         <div className='shop-container'>
             <div className="products-container">
@@ -17,11 +25,12 @@ const Shop = () => {
                 products.map(product => <Product
                 key = {product.id}
                 product = {product}
+                handleAddToCart = {handleAddToCart}
                 > </Product> )
             }
             </div>
             <div className="cart-container">
-                <h3>Order summary</h3>
+                <h3>Order summary :{cart.length} </h3>
             </div>
           
         </div>
